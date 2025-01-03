1a5b77e02e863a058b5494d7260d7e691e80a635
Semih Irmak <smhirmak@hotmail.com>
2025-01-03 22:31:11 +0300

    fix intro animation

--- src/components/PreLoader/PreLoader.tsx
+++ src/components/PreLoader/PreLoader.tsx
@@ -7,7 +7,7 @@ const PreLoader = () => {
   }, []);
   return (
     <div className="preloader h-screen w-full bg-black text-white fixed bottom-0 left-0 right-0 top-0 z-50 flex justify-center items-center overflow-hidden overflow-y-hidden">
-      <div className="texts-container flex flex-col items-center justify-between h-16 w-[350px] overflow-hidden">
+      <div className="texts-container opacity-0 flex flex-col items-center justify-between h-16 w-[350px] overflow-hidden">
         <span className='text-xl font-extrabold text-white'>Muhammed Semih Irmak</span>
         <span className='text-xl font-extrabold text-white'>Frontend Developer</span>
       </div>
